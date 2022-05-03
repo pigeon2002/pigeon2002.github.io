ed020369bb7f788aa0c1ff5e47b6c1d919e1c69b
pigeon2002 <104818979+pigeon2002@users.noreply.github.com>
2022-05-03 13:47:32 +0700

    Com graphic

---
+++ Template Final/main.cpp
@@ -0,0 +1,11 @@
+#include<windows.h>
+#include<GL\glut.h>
+#include<time.h>
+#include<math.h>
+
+int main(int argc,char** argv)
+{
+    glutInit(&argc,argv); //initialize toolkit
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+
+}
